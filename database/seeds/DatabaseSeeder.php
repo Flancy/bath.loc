7631644a14bc859386ca1acda5956e541a051769
Trifonov Kirill <flancyk.flancyk@gmail.com>
2020-06-17 01:07:15 +0300

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(TrainersTableSeeder::class);
-        $this->call(ChildrensTableSeeder::class);
+        //$this->call(TrainersTableSeeder::class);
+        //$this->call(ChildrensTableSeeder::class);
     }
 }
